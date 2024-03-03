6e852023b3c8883457639f08513ad020ca9ed095
brianschnee <brian.a.schnee@gmail.com>
2024-03-03 18:37:48 -0500

    testing single token lexing

--- src/lexer.rs
+++ src/lexer.rs
@@ -1,6 +1,5 @@
-use std::str::Chars;
-
 use crate::token::Token;
+use std::str::Chars;
 
 #[derive(Debug)]
 pub struct Lexer<'a> {
@@ -34,8 +33,17 @@ mod tests {
     use super::*;
 
     #[test]
+    fn should_parse_a_single_token() {
+        let token = Lexer::new("+").next_token().unwrap();
+        assert_eq!(token, Token::Plus);
+
+        let token = Lexer::new("-").next_token().unwrap();
+        assert_eq!(token, Token::Minus);
+    }
+
+    #[test]
     fn should_parse_single_character_tokens() {
         let mut lexer = Lexer::new("+=  -/* !");
-        todo!("test next token functionality");
+        todo!("parsing single character tokens for an entire string")
     }
 }
